49f67942dfde0639ea6a348c8c22dfb4e0910054
Shehab Ahamed Chowdhury Mitul <121333557+Mitul3047@users.noreply.github.com>
2023-08-28 14:47:47 +0600

    index and js file added

---
+++ js/phone.js
@@ -0,0 +1,74 @@
+const loadPhone = async (searchText) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
+    const data = await res.json();
+    // console.log(data)
+    // console.log(data.data)
+    const phones = data.data
+    displayPhones(phones);
+}
+
+
+const displayPhones = phones => {
+    console.log(phones)
+
+    const phoneContainer = document.getElementById('phone-container')
+    // clear container before new serach
+    phoneContainer.textContent ="";
+    console.log(phones.length)
+    // show all buton
+const showAllButton = document.getElementById('show-all-btn')
+    if (phones.length > 12) {
+        showAllButton.classList.remove('hidden')
+    }else{
+        showAllButton.classList.add('hidden')
+    }
+
+    // display 1ST 12 phone
+    // phones = phones.slice(0,5)
+    phones = phones.slice(0,12)
+
+
+    phones.forEach(phone => {
+        console.log(phone)
+        // make div
+
+        const phoneCard = document.createElement('div');
+        // make class
+        phoneCard.classList = `card p-4 bg-gray-100 shadow-xl`;
+        //    make inner html
+        phoneCard.innerHTML = `
+        <figure><img src="${phone.image}" /></figure>
+                    <div class="card-body">
+                        <h2 class="card-title">${phone.phone_name}</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div class="card-actions justify-end">
+                            <button class="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+        `
+        // append
+        phoneContainer.appendChild(phoneCard)
+    });
+}
+
+// handle search
+ const handleSearch = () =>{
+    // console.log(2345)
+const searchField = document.getElementById('search-field');
+const searchText = searchField.value;
+// console.log(searchText)
+loadPhone(searchText);
+}
+
+const handleSearch2 = () =>{
+    // console.log(2345)
+const searchField = document.getElementById('search-field2');
+const searchText = searchField.value;
+loadPhone(searchText);
+console.log(searchText)
+}
+
+
+// loadPhone()
+
+console.log("1232")
